098037ad56df6b79f84c7deefe66c76caab07a77
isacvale <isac@ocastudios.com>
2019-11-17 12:25:21 -0300

    Accepts milliseconds as paramerter for timestamp.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express()
 
 app.get('/api/timestamp/:date_string?', (req, res) => {
   const date = req.params.date_string
-    ? new Date(req.params.date_string)
+    ? isNaN(+req.params.date_string)
+      ? new Date(req.params.date_string)
+      : new Date(+req.params.date_string)
     : new Date()
   isNaN(date)
     ? res.json({ error: 'Invalid Date' })
