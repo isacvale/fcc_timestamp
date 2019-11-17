32d5dc55ce4bd647f8682a69e9eb7b3d6ee9acf4
isacvale <isac@ocastudios.com>
2019-11-16 21:38:40 -0300

    Begins

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  const date = req.params.date_string
+    ? new Date(req.params.date_string)
+    : new Date()
+  isNaN(date)
+    ? res.json({ error: 'Invalid Date' })
+    : res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+})
+
+app.get("/", function(request, response) {
+  response.send('naah');
+});
+
+app.listen(8000, () =>
+  console.log('alive@8000')
+)
