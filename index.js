c9945d2f12df5eeb808b0ab61d94ef3a188fd5d1
isacvale <isac@ocastudios.com>
2019-11-17 14:07:54 -0300

    Add header api point.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({optionSuccessStatus: 200}))
 
+// Timestamp project
 app.get('/api/timestamp/:date_string?', (req, res) => {
   console.log('input:', req.params.date_string)
   const date = req.params.date_string
@@ -17,6 +18,15 @@ app.get('/api/timestamp/:date_string?', (req, res) => {
       unix: date.getTime(),
       utc: date.toUTCString()
     })
+})
+
+// Header project
+app.get('/api/header', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get("Accept-Language"),
+    software: req.get("User-Agent")
+  })
 })
 
 app.get("/", function(request, response) {
