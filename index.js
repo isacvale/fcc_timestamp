31aa765aebfd5033a92bd66ba21081db90602ab4
isacvale <isac@ocastudios.com>
2019-12-24 00:18:37 -0300

    Draft log exercise.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const dns = require('dns')
 const mongoose = require('mongoose')
 const uuid = require('uuid/v4')
 
-
+const app = express()
 app.use(cors({optionSuccessStatus: 200}))
 app.use('/web/pages', express.static(process.cwd()+'/web/pages'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 mongoose.connection.once('open', () =>
   console.log("Connection Successful!")
 )
 
-// Timestamp project
+/*
+ Timestamp project
+ ***************************************************************/
 app.get('/api/timestamp/:date_string?', (req, res) => {
   const date = req.params.date_string
     ? isNaN(+req.params.date_string)
@@ -31,7 +35,9 @@ app.get('/api/timestamp/:date_string?', (req, res) => {
     })
 })
 
-// Header project
+/*
+ Header project
+***************************************************************/
 app.get('/api/header', (req, res) => {
   res.json({
     ipaddress: req.ip,
@@ -40,7 +46,9 @@ app.get('/api/header', (req, res) => {
   })
 })
 
-// URL Shortener Microservice
+/*
+ URL Shortener Microservice
+***************************************************************/
 const urlSchema = new mongoose.Schema({
   original: String,
   short: String,
@@ -90,8 +98,9 @@ app.get('/api/shorturl/:short', (req, res) => {
   })
 })
 
-/*In order to avoid exploits, as a protective measure, every time
- data is entered, we delete anyone older than 2 minutes.
+/*
+  As a protective measure in order to avoid exploits, every time
+  data is entered, we delete anyone older than 2 minutes.
 */
 function flushOldData () {
   const oldEntries = urlModel.deleteMany({
@@ -106,12 +115,243 @@ function addMinutes (date, minutes) {
   return new Date(date.getTime() + minutes * 60000)
 }
 
-// Default home
-app.get("/", function(request, response) {
+/*
+ Exercise tracker
+***************************************************************/
+const exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: Date,
+  creationTime: Date
+})
+const personSchema = new mongoose.Schema({
+  name: String,
+  userId: String,
+  exercises: [exerciseSchema],
+  creationTime: Date
+})
+
+const personModel = mongoose.model('personModel', personSchema)
+const exerciseModel = mongoose.model('exerciseModel', exerciseSchema)
+
+// Homebase: send ui page
+app.get('/api/exercise/', (req, res) =>
+  res.sendFile(process.cwd()+'/web/pages/exercise_tracker.html')
+)
+
+const replaceAll = (text, remove, add) => text.replace(new RegExp(remove, 'g'), add)
+
+// Adds a new person
+app.post('/api/exercise/new-user', (req, res) => { //res.send('adds new user {username}, get obj user'))
+  const { body } = req
+  const { username } = body
+
+  const newPerson = ({
+    name: username,
+    exercises: [],
+    userId: replaceAll(uuid(), '-', ''),
+    creationTime: new Date()
+  })
+
+  const entry = new personModel(newPerson)
+  entry.save()
+  res.json(newPerson)
+})
+
+// Adds a new exercise to a person
+app.post('/api/exercise/add', async (req, res) => { // res.send('ads new exercise {userId, description, duration} [date=now], get obj user with exercises'))
+  const { body } = req
+  const { userId, duration, date, description } = body
+
+  personModel.findOne({ userId: userId }, async (err, person) => {
+    if (err) console.log('error:', err)
+    person.exercises = [
+      ...person.exercises,
+      {
+        description,
+        date: date || new Date(),
+        duration,
+        creationTime: new Date()
+      }
+    ]
+    person.save( err => err
+      ? console.log(err)
+      : console.log('save successful')
+    )
+    res.json(person)
+  })
+})
+
+// http://localhost:5000/api/exercise/log?userId=c2e6ea73acac4a78b8b9206dfb356182
+// http://localhost:5000/api/exercise/log/:userId/:dateFrom?/:dateTo?/:limit?'
+
+getISODate = dateString => new Date(dateString)//.toISOString()
+
+// Logs a person's exercise list with optional filters
+app.get('/api/exercise/log', (req, res) => {
+
+  const { query } = req
+  let { userId, dateFrom, dateTo, limit } = query
+
+  dateFrom = '2012-03-05'
+  dateTo = '2022-03-05'
+  limit = 0
+
+  let filterMatch = { $match: { userId } }
+  if (dateFrom)
+    safeAppend2(filterMatch, ['$match', 'exercises.date', '$gte', new Date(dateFrom)])
+  if (dateTo)
+    safeAppend2(filterMatch, ['$match', 'exercises.date', '$lte', new Date(dateTo)])
+
+console.debug('lim', limit)
+
+  personModel.aggregate(
+    [
+      filterMatch,
+      limit ? { $limit: +limit } : null
+    ].filter(Boolean),
+    function (err, data) {
+      if (err)
+        console.debug('err', err)
+      else {
+        let resData
+        if (data.length) { // there's a match
+          resData = {
+            userId: data[0].userId,
+            name: data[0].name,
+            count: data[0].exercises ? data[0].exercises.length : 0,
+            log: data[0].exercises.map(item => ({
+              description: item.description,
+              duration: item.duration,
+              date: item.date
+            }))
+          }
+        }
+        res.json(resData)
+      }
+    }
+  )
+})
+
+
+
+// // Logs a person's exercise list with optional filters
+// app.get('/api/exercise/log', (req, res) => { // res.send('gets list of exercise, gets user obj + exercises + exercise count'))
+// // app.get('/api/exercise/log/:userId/:dateFrom?/:dateTo?/:limit?', (req, res) => { // res.send('gets list of exercise, gets user obj + exercises + exercise count'))
+//   // res.json(req.query)
+
+//   const { query } = req
+//   let { userId, dateFrom, dateTo, limit } = query
+
+//   // const filters =  { userId }
+//   // if (dateFrom || dateTo) filters.exercises = {}
+//   // if (dateFrom && dateTo) filters.exercises.date = {$gte: getISODate(dateFrom), $lte: getISODate(dateTo)}
+//   // else if (dateFrom) filters.exercises.date = {$gte: getISODate(dateFrom)}
+//   // else if (dateTo) filters.exercises.date = {$lte: getISODate(dateTo)}
+
+//   // console.log('query:', query)
+//   // console.log('filters:', filters)
+//   // res.json({query: query, filters: filters})
+
+//   dateFrom = '2012-03-05'
+//   dateTo = '2022-03-05'
+
+//   let filterMatch = { $match: { userId } }
+//   if (dateFrom)
+//     safeAppend2(filterMatch, ['$match', 'exercises.date', '$gte', getISODate(dateFrom)])
+//   console.debug('filters 1', filterMatch)
+//   if (dateTo)
+//     safeAppend2(filterMatch, ['$match', 'exercises.date', '$lte', getISODate(dateTo)])
+
+//   console.debug('filters 2', filterMatch)
+//   // res.json(filterMatch)
+//   personModel.aggregate([
+//       filterMatch
+//     ], function (err, res) {
+//       if (err) console.debug('err', err)
+//       else
+//       // console.log('res', res)
+//       res.json(res || [err])
+//     })
+// })
+
+
+safeAppend = (obj, argList, curObj) => {
+  if (argList.length) {
+    const newList = [...argList]
+    const lastItem = newList.pop()
+    console.log('pop', lastItem)
+    if (!curObj)
+      return safeAppend(obj, newList, lastItem)
+    else
+      return safeAppend(obj, newList, {[lastItem]: curObj})
+  }
+  return { ...obj, ...curObj}
+}
+
+safeAppend2 = (obj, argList) => {
+  argList.reduce(safePathReducer, obj)
+  // const newargList = [...argList]
+  // let curObj = obj
+  // let newItem
+  // let oldItem
+  // let oldObj
+
+  // while (newargList.length > 1) {
+  //   oldItem = newItem
+  //   newItem = newargList.shift()
+
+  //   if (!curObj[newItem]) curObj[newItem] = {}
+
+  //   oldObj = curObj
+  //   curObj = curObj[newItem]
+  // }
+
+  // oldObj[newItem] = newargList[0]
+}
+
+safePathReducer = (acc, cur, idx, arr) => {
+  guardAddProp = (obj, prop) => obj[prop] ? null : obj[prop] = {}
+
+  // If first loop: set up rich accumulator
+  if (idx === 0) {
+    guardAddProp(acc, cur)
+    return { obj: acc, ref: [acc, cur] }
+  }
+
+  // Other loops
+  else {
+    const refObj = acc.ref[0]
+    const refProp = acc.ref[1]
+
+    // Regular loops
+    if (idx < arr.length - 1) {
+      guardAddProp(refObj[refProp], cur)
+      return { obj: acc.obj, ref: [refObj[refProp], cur] }
+    }
+
+    // Last loop
+    else {
+      refObj[refProp] = cur
+      return acc.obj
+    }
+  }
+}
+
+/*
+ Default home
+ ***************************************************************/
+
+ app.get("/", function(request, response) {
   response.send('naah');
 });
 
 app.listen(
-  process.env.PORT ? process.env.PORT : 8000, () =>
-  console.log(`alive@${process.env.PORT ? process.env.PORT : 8000}`)
+  process.env.PORT
+    ? process.env.PORT
+    : 8000,
+    () =>
+      console.log(
+        `alive@${process.env.PORT ? process.env.PORT : 8000}`
+      )
 )
